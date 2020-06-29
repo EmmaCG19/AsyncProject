0f66bdd2f79a5ef885f978683a0a15c1dfaef5c9
EmmanuelF <emmanuelfernandez600@gmail.com>
2020-06-29 19:09:16 -0300

    Algun save equivocada en Visual Studio

---
+++ AsyncMethods/WebpagesMVC/Controllers/WebpagesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebpagesMVC.Models;
+
+namespace WebpagesMVC.Controllers
+{
+    public class WebpagesController : Controller
+    {
+        [HttpGet]
+        public ActionResult ObtenerSitiosWeb()
+        {
+            WebPageVM model = new WebPageVM();
+            ViewBag.Title = "Lista de websites";
+
+            return View(model); 
+        }
+
+        
+    }
+}
